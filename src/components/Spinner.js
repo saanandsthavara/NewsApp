ff25a1f3d8fad417395adc84028001ca336a7297
Saanand Sthavara <saanand@protonmail.com>
2021-09-21 01:58:24 +0530

    Using Funtional Based

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react'
-import loading from './loading.gif'
+import React from "react";
 
-export class Spinner extends Component {
-    render() {
-        return (
-            <div className="text-center">
-                <img className="my-3" src={loading} alt="loading" />
-            </div>
-        )
-    }
-}
+import loading from "./loading.gif";
 
-export default Spinner
+const Spinner = (props) => {
+  return (
+    <div className="text-center">
+      <img className="my-3" src={loading} alt="loading" />
+    </div>
+  );
+};
+
+export default Spinner;
